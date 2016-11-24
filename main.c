1b3010509a24ac715d9c01f0a5938639420642be
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-11-24 01:05:18 -0200

    feat: hex representation

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
+int main() {
   FILE *file;
-  char buffer[10];
-  file = fopen("samples/pure_text.txt", "rb");
-  while(!feof(file)) {
-    fread(buffer, sizeof(*buffer), sizeof(buffer) / sizeof(*buffer), file);
-    printf("%04X\n", buffer);
-  }
+  char buffer[11];
+  int i;
+  file = fopen("samples/L1M0MAP1.PCX", "rb");
+  fread(buffer, 1, 12, file);
   fclose(file);
 
-  for (int i = 0; i < sizeof(buffer); i++) {
-    printf("%04x",buffer[2]);
+  printf("Here is file's hex:\n\n");
+
+  for (i = 0; i < 12; i++) {
+    printf("%02X ",buffer[i]);
   }
   //todo: read PCX header
   //todo: read PCX data
