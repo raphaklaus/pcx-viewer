788afdcb1384708b23ae5faa392dfc3a0b4b4ff2
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-11-23 23:58:17 -0200

    feat: reading hex chunks

--- main.c
+++ main.c
@@ -2,13 +2,17 @@
 
 int main(int argc, char const *argv[]) {
   FILE *file;
-  char buffer[2];
-  file = fopen("samples/L1M0MAP1.PCX", "rb");
+  char buffer[10];
+  file = fopen("samples/pure_text.txt", "rb");
   while(!feof(file)) {
-    fread(buffer, 2, (sizeof buffer) -1, file);
-    printf("%X", buffer);
+    fread(buffer, sizeof(*buffer), sizeof(buffer) / sizeof(*buffer), file);
+    printf("%04X\n", buffer);
   }
   fclose(file);
+
+  for (int i = 0; i < sizeof(buffer); i++) {
+    printf("%04x",buffer[2]);
+  }
   //todo: read PCX header
   //todo: read PCX data
   return 0;
